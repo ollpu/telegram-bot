b258f692b56ad5e37f14459a0ff852d3fad27c37
Roope Salmi <ollpu.github@zah.fi>
2015-03-15 12:04:26 +0200

    Added reference to GitHub repo with !info command

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,7 +1,8 @@
 
 function run(msg, matches)
   return [[Telegram Bot (modified by @ollpu)
-Currently running on a Raspberry Pi.]]
+Currently running on a Raspberry Pi.
+GitHub repo: github.com/ollpu/telegram-bot]]
 end
 
 return {
