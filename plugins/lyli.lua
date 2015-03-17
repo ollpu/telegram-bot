0d98bb65f93c0ecf64a4445c13eee85a279ef9a9
Roope Salmi <rpsalmi@gmail.com>
2015-03-17 21:23:43 +0200

    Simplify URL parsing in lyli.lua

--- plugins/lyli.lua
+++ plugins/lyli.lua
@@ -29,7 +29,7 @@ function run(msg, matches)
         results = lyliit(captured_URL_table[to_id])
       end
     end
-  elseif string.match(msg.text, "\b(https?://[-A-Z0-9+&@#/%?=~_|!:,.;]*)") and not string.match(msg.text, "^❱") then
+  elseif string.match(msg.text, "\b(https?://[^ ]*)") and not string.match(msg.text, "^❱") then
     print("Found URL!")
     results = catch_url(msg)
   end
@@ -59,7 +59,7 @@ return {
   patterns = {
     "^!lyli (.*)$",
     "^!pili (.*)$",
-    "\b(https?://[-A-Z0-9+&@#/%?=~_|!:,.;]*)",
+    "\b(https?://[^ ]*)",
     "^!lyli$"
   },
   run = run
